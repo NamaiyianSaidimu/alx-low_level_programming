ea9bcad76725d750e03dac4fede88ce49735d0ec
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-17 12:35:47 -0800

    A C program that prints exactly with proper grammar, but the outcome is a piece of art, using the printf function.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main - Our program's entry point.
+ *
+ * Description: Printinf a specified statement
+ * using printf
+ *
+ * Return: Always 0 (Success!)
+ */
 int main(void)
 {
 	/*printing a specified line of text:*/
