ed4cc3ca19d02286b11961578c235f68d2d80df6
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-18 12:44:37 -0800

    A C program that prints the alphabet in lower case using the putchar function.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - out program's entry point
+ *
+ * description: A program that prints the
+ * alphabet in lower case using the
+ * function putchar()
+ *
+ * Return: Always 0(Success!)
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha='a';alpha<='z';alpha++)
+	{
+		putchar(alpha);
+	}
+	return(0);
+}
