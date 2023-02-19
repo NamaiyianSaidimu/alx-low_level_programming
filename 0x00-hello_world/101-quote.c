8ee23a1594b8ec234f4db2ef6b4fdfcb0d0c450a
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-19 13:53:04 -0800

    A C program that prints a specified string to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - our program's entry point.
+ *
+ * description: A program that prints a
+ * specified string to the standard
+ * output.
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" "
+	"- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
