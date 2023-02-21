9f1459682eb82d0e38e4de702995b591c1852814
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-21 06:29:18 -0800

    Our project's header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#include<unistd.h>
+ 
+/**
+ * _putchar: A function that prints a character
+ * to the standard output.
+ *
+ * Description: The character c is what will be
+ * printed to the stdout.
+ *
+ * Return: 1 on success.
+ * On error, -1 will be returned and errno will be set appropriately.
+ */
+int putchar(char c)
+{
+	return (write(1, &c, 1));
+}
