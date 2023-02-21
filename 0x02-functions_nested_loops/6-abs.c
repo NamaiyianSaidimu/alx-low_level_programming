28d01ca5390e8ac85fcbfd955c0c28d583c62b60
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-21 14:07:07 -0800

    A C function that prints the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,16 +2,12 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n > 0 || n == 0)
 	{
-		_putchar(n + '0');
-	}
-	else if (n == 0)
-	{
-		_putchar(n + '0');
+		return(n);
 	}
 	else
 	{
-		_putchar((n * -1) + '0');
+		return(n * -1);
 	}
 }
