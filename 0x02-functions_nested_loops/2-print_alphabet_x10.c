7c0a769c0fa428d37e12b1617ff7704d78df8733
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-21 12:44:06 -0800

    A C function that prints ten times the alphabet in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10  print 10 times the alphabet
+ * print_alphabet_x10 - print 10 times the alphabet
  *
  * description: This function prints ten times the
  * alphabet in lowercase, followed by a new line.
