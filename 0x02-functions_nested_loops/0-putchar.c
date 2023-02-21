3359288303917b85b948ad42d4fcc5f813b85831
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-21 04:37:57 -0800

    A C program that prints _putchar followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+
+/** main - our function's entry point.
+ *
+ * Description: a program that prints _putchar;
+ *
+ * Return: 0(Success!)
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("a");
+	_putchar("r");
+	return (0);
+}
