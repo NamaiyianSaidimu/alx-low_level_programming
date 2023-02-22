198caa5452a5e9212c476f300c225e83cba9f17c
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-22 02:57:38 -0800

    A C function that prints the absolute value of a number.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,14 @@
 
 /**
  * _abs - function for absolute values.
+ * @n - the variable that will store the
+ * number to be tested.
  *
  * description: It computes the absolute value
  * of a number.
  *
  * Return: absolute values.
  */
-
 int _abs(int n)
 {
 	if (n > 0 || n == 0)
