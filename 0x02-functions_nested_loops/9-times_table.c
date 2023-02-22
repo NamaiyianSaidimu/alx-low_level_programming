343b8e959cc9a651e88215f6256b9e180bf6c1c8
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-22 12:44:36 -0800

    A C program that prints the 9 times table.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - printing the 9 times table
+ *
+ * Description: We are printing the entire
+ * multiplication table from 0 to 9
+ *
+ * Return: no value.
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i < 9)
+		{
+			_putchar(0 + '0');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(0 + '0');
+		}
+	}
+	for (i = 0; i <= 9; i++)
+	{
+		if (i < 9)
+		{
+			_putchar(i * (i + 1) + '0');
+			_putchar(' ')'
+		}
+		else
+		{
+			_putchar(i * (i + 1) + '0');
+			_putchar(' ');
+		}
+	}
+}
