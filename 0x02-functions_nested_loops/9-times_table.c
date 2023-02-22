d7fdd100e14d3f73487d11abce835feb995c1d59
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-22 13:11:04 -0800

    A C program that prints the 9 times table.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,13 +30,13 @@ void times_table(void)
 	{
 		if (i < 9)
 		{
-			_putchar(i * (i + 1) + '0');
+			_putchar((i * (i + 1)) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
 		else
 		{
-			_putchar(i * (i + 1) + '0');
+			_putchar((i * (i + 1)) + '0');
 			_putchar('\n');
 		}
 	}
