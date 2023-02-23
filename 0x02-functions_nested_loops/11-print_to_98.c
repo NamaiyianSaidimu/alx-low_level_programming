532ea1242bf3cb365a5cff60d3b1ad40424329fc
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-22 16:00:22 -0800

    A function that prints from integer n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,8 @@
  *
  * Return: no value.
  */
-void print_to_98(void)
+int print_to_98(int n)
 {
-	int n;
-	printf("Enter a number: \n");
-	scanf("%d", &n);
-
 	if (n > 98)
 	{
 		for (; n >= 98; n--)
