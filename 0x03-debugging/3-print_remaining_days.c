553cc93600702fcc03ba55c58fb4f2bca527f014
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-23 13:57:41 -0800

    A C function that converts a date to the day of the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,6 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2)
 		{
-			convert_day(month, day);
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
@@ -26,12 +25,10 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			convert_day(month, day);
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			convert_day(month, day);
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365-day);
 		}
