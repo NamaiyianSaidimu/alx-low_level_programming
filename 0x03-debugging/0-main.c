af8b665737566d2ea1e6020e22a05d21ae169afa
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-23 03:55:58 -0800

    Testing a function that prints out whether a number is positive or negative.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - our program's entry point.
+ *
+ * description: determining whether a number
+ * is positive or negative or zero.
+ *
+ * Return: 0(Success!)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
