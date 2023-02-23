2eba0c404ab04653d5212a6130c089c2b8b8e717
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-23 14:46:37 -0800

    Our project's header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,8 +5,9 @@
 
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
-
 int convert_day(int month, int day);
 
 void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
+
