6d199c5074413eb4ab4ba08b1ad9e26be5816bcd
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-02-27 00:46:16 -0800

    A function in C that checks for uppercase letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,10 @@
  * is_upper - function that checks for uppercase characters
  * @c: character to be evaluated.
  *
+ * Description: A character c is passed in the function
+ * and the function checks whether it is uppercase of
+ * lowercase.
+ *
  * Return: 1 if c is uppercase
  * Otherwise, 0 is returned.
  */
