7cf0bf4b550d60f1e79df221b71d8e34abbe2b83
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-03-02 12:09:08 -0800

    A C function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1)
+	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
 		{
-			break;
+			return (*s1 - *s2);
 		}
 		s1++;
 		s2++;
 	}
-	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+	return (0);
 }
 
