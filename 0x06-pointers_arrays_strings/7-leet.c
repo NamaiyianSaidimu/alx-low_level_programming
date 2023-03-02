3aa6939d2a47e3266f9400a7f7e54a436f688e51
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-03-02 10:10:49 -0800

    A Function to encode a string using leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,29 +10,20 @@
 char *leet(char *s)
 {
 	int i;
+	char *new = s;
+	char letters[] = {'A', 'E', 'O', 'T', 'L'};
+	char numbers[] = {4, 3, 0, 7, 1};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while(*s)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (i = 0; i < 5; i++)
 		{
-			s[i] = '4';
+			if (*s == letters[i] || *s == letters[i] +32)
+			{
+				*s = numbers[i] + '0';
+			}
 		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
+		s++;
 	}
-	return (s);
+	return (new);
 }
