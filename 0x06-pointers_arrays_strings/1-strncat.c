9301c8b078fb6878fad5444a9de306ec106519a8
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-03-02 01:02:57 -0800

    C function to concatenate two strings, with comments

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,15 +14,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int len = 0;
 	char *ptr;
-
+	/* Finding the length of string *dest */
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		len++;
 	}
-
+	/* Pointing to the last character of *dest */
 	ptr = dest + len;
 
-
+	/* Adding the src characters to dest */
 	while (*src != '\0' && n--)
 	{
 		*ptr++ = *src++;
