e91d62b6c270aa19e3c8ff8c6df0a1ccad2c4484
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-03-14 06:58:02 -0700

    A C function that returns a pointer to a 2d array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2d array
+ * @width: array width
+ * @height: array height
+ *
+ * Return: piinter to a 2d array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array_2d;
+	int i;
+	int j;
+
+	array_2d = malloc(sizeof(int) * height);
+	for (i = 0; i < height; i++)
+	{
+		array_2d[i] = malloc(sizeof(int) * width);
+	}
+
+	if (width <= 0 && height <= 0)
+	{
+		return (NULL);
+	}
+	if (array_2d == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				*(*(array_2d + i) + j) = 0;
+			}
+		}
+		return (array_2d);
+	}
+}
