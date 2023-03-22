a4c6aa18b91eb1cffdc5c98b5187407cbe93eaca
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-03-22 14:24:31 -0700

    Adding stdlib header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - printing a name
  * @name: The name to print
- * @f: a function to store our function
+ * @f: a function pointer
  *
  * Return: void
  */
