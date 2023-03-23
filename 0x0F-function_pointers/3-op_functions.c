79f601ba9aefcb319be2b2b870775845efd04476
NamaiyianSaidimu <graceysaidimu@gmail.com>
2023-03-22 17:05:38 -0700

    A file contatining all the functions for somple arithmetic

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adding two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracting two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: subtraction result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplying two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - dividing two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finding the modulo of 2 integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Modulo result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
